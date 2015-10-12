05f93b42afc84a5d06353c78aa0305f3dd292b00
Cosmin Dumitru <cosu@cosu.ro>
2015-10-12 19:45:50 +0200

    add das config

---
+++ scripts/vampires_das.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=00:15:00
+#SBATCH --ntasks-per-node=1
+
+. /etc/bashrc
+. /etc/profile.d/modules.sh
+module load java/jdk-1.8.0
+
+#bind on infiniband
+export BIND_IP=$(ip -f inet addr show ib0 |grep inet |awk '{print $2}'|cut -f1 -d/)
+
+#server ip is given as first argument
+export SERVER_IP=$1
+shift
+
+srun /home/cosmin/vampires-akka/client/build/install/client/bin/client "$@"
