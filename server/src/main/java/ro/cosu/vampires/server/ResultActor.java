870d0be39a323388d8d1b134e4838c795d7e8096
Cosmin Dumitru <cosu@cosu.ro>
2015-10-16 08:12:05 +0200

    write results to json

--- server/src/main/java/ro/cosu/vampires/server/ResultActor.java
+++ server/src/main/java/ro/cosu/vampires/server/ResultActor.java
@@ -4,19 +4,53 @@ import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ResultActor extends UntypedActor{
     private LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 
+    private List<Message.Result> results = new LinkedList<>();
 
     public static Props props(){
         return Props.create(ResultActor.class);
     }
 
+
+    public  void preStart() {
+        getContext().actorSelection("/user/terminator").tell(new Message.Up(), getSelf());
+    }
+
+    @Override
+    public void postStop(){
+        //write results to disk
+        try {
+
+            Writer writer = new FileWriter(Paths.get(System.getProperty("user.home"), "results.json").toFile());
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            gson.toJson(results, writer);
+
+        } catch (IOException e) {
+            e.printStackTrace();  //Auto-generated TODO
+        }
+
+
+    }
+
     @Override
     public void onReceive(Object message) throws Exception {
         if (message instanceof Message.Result) {
-            log.info("got result {}", message);
+            results.add((Message.Result) message);
+            log.debug("got result {}", message);
         }
     }
 }
