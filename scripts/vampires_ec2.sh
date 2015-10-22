d250e0b341ad8a26a7917f83a56dfa24b6a4412a
Cosmin Dumitru <cosu@cosu.ro>
2015-10-23 01:46:59 +0200

    config for ec2

--- scripts/vampires_ec2.sh
+++ scripts/vampires_ec2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export BIND_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+export BIND_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 export SERVER_IP=$1
 shift
 
