a7850cbbb0f5a57868014b9a858e7da0e2246922
Cosmin Dumitru <cosu@cosu.ro>
2015-10-23 02:31:19 +0200

    cleanup config

--- scripts/vampires_das.sh
+++ scripts/vampires_das.sh
@@ -7,7 +7,7 @@
 module load java/jdk-1.8.0
 
 #bind on infiniband
-export BIND_IP=$(ip -f inet addr show ib0 |grep inet |awk '{print $2}'|cut -f1 -d/)
+export HOST=$(ip -f inet addr show ib0 |grep inet |awk '{print $2}'|cut -f1 -d/)
 
 #server ip is given as first argument
 export SERVER_IP=$1
