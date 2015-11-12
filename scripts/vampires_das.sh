c34ab99c6adb11d3a6c8a8de40b0286a763f5fba
Cosmin Dumitru <c.dumitru@sig.eu>
2015-11-13 00:26:52 +0100

    fix das scripts

--- scripts/vampires_das.sh
+++ scripts/vampires_das.sh
@@ -13,4 +13,4 @@ export HOST=$(ip -f inet addr show ib0 |grep inet |awk '{print $2}'|cut -f1 -d/)
 export SERVER_IP=$1
 shift
 
-srun ${HOME}/vampires-akka/client/build/install/client/bin/client "$@"
+srun ${HOME}/vampires-akka/client/bin/client "$@"
