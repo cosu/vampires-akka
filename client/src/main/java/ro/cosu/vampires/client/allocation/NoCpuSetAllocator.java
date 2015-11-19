afd5c9e924f47209298b7956863cad52c3018d16
Cosmin Dumitru <c.dumitru@sig.eu>
2015-11-19 22:03:22 +0100

    separate integration tests

--- client/src/main/java/ro/cosu/vampires/client/allocation/NoCpuSetAllocator.java
+++ client/src/main/java/ro/cosu/vampires/client/allocation/NoCpuSetAllocator.java
@@ -1,4 +1,15 @@
 package ro.cosu.vampires.client.allocation;
 
-public class NoCpuSetAllocator {
+import java.util.Optional;
+
+public class NoCpuSetAllocator implements CpuAllocator {
+    @Override
+    public Optional<CpuSet> acquireCpuSet() {
+        return Optional.empty();
+    }
+
+    @Override
+    public void releaseCpuSets(CpuSet cpuSet) {
+        // nothing
+    }
 }
