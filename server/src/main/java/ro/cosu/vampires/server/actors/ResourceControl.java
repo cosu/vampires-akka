e169170f0818e1572676d260b4b810e392059930
Cosmin Dumitru <c.dumitru@sig.eu>
2015-11-23 23:38:41 +0100

    add tostring to resource control

--- server/src/main/java/ro/cosu/vampires/server/actors/ResourceControl.java
+++ server/src/main/java/ro/cosu/vampires/server/actors/ResourceControl.java
@@ -6,19 +6,34 @@ import java.io.Serializable;
 
 public class ResourceControl {
 
-    public static class Shutdown {
+    public static class Shutdown implements Serializable {
     }
 
-    public static class Up implements  Serializable{}
+    public static class Up implements Serializable {
+    }
+
+    public static class Destroy implements Serializable {
+    }
+
+    public static class Info implements Serializable {
+    }
 
 
     public static class Create implements Serializable {
         final Resource.Type type;
-        final Resource.Parameters  parameters;
+        final Resource.Parameters parameters;
 
         public Create(Resource.Type type, Resource.Parameters parameters) {
             this.type = type;
             this.parameters = parameters;
+        }
+
+        @Override
+        public String toString() {
+            return "Create{" +
+                    "type=" + type +
+                    ", parameters=" + parameters +
+                    '}';
         }
     }
 
@@ -30,18 +45,15 @@ public class ResourceControl {
             this.type = type;
             this.name = name;
         }
+
+        @Override
+        public String toString() {
+            return "Start{" +
+                    "type=" + type +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
     }
-
-
-    public static class Destroy implements Serializable  {
-    }
-
-    public static class Info implements Serializable  {
-
-    }
-
-
-
 
 
 }
