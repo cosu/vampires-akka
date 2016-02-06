cfdf0ec76073c4cd65d8a9814f494b70a0ce4d7f
Cosmin Dumitru <cosu@cosu.ro>
2016-02-06 19:17:43 +0100

    set kamon sigar path

--- scripts/vampires_ec2.sh
+++ scripts/vampires_ec2.sh
@@ -5,5 +5,6 @@ export BIND_HOST=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 export SERVER_IP=$1
 export HOME=/home/ubuntu
 shift
+export KAMON_SIGAR_FOLDER=/tmp
 
-/home/ubuntu/vampires-akka/client/build/install/client/bin/client -Dkamon.sigar.folder=/tmp "$@"
+/home/ubuntu/vampires-akka/client/build/install/client/bin/client "$@"
