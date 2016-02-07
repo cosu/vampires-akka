368ea2ccb6b858d6c019bb85430acb391d8db9ab
Cosmin Dumitru <cosu@cosu.ro>
2016-02-08 00:24:09 +0100

    update server script

--- scripts/vampires_server.sh
+++ scripts/vampires_server.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-export BIND_HOST=`curl http://ip.cosu.ro/?ip`
+IP=`curl http://ip.cosu.ro/?ip`
+export BIND_HOST=${IP}
+export HOST="0.0.0.0"
 export SERVER_OPTS="-Dconfig.file=$1"
 shift
-echo "Binding to ${BIND_HOST}"
+echo "Binding to ${BIND_HOST} ${HOST}"
 
 VAMPIRES_SERVER_HOME="${HOME}/vampires-akka/server/"
 
