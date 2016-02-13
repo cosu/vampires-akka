5f8d813f5326505743b9b1381f8334a6beb3d632
Cosmin Dumitru <cosu@cosu.ro>
2016-02-14 00:03:11 +0100

    add job builders tests

--- server/src/test/java/ro/cosu/vampires/server/workload/JobTest.java
+++ server/src/test/java/ro/cosu/vampires/server/workload/JobTest.java
@@ -2,6 +2,7 @@ package ro.cosu.vampires.server.workload;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -9,11 +10,41 @@ public class JobTest {
     @Test
     public void testWorkloadBuilder() throws Exception {
 
-        Job build = Job.builder()
-                .computation(Computation.builder().command("test").id("10").build())
-                .metrics(Metrics.empty())
-                .result(Result.empty())
-                .build();
+        Job build = getJob();
         assertThat(build.computation().id(), is("10"));
     }
+
+    private Job getJob() {
+        return Job.builder()
+                    .computation(Computation.builder().command("test").id("10").build())
+                    .metrics(Metrics.empty())
+                    .result(Result.empty())
+                    .build();
+    }
+
+    @Test
+    public void testWithResult() throws Exception {
+        Job job = getJob().withResult(Result.empty());
+        assertThat(job.status(), is(JobStatus.EXECUTED));
+    }
+
+    @Test
+    public void testWithMetrics() throws Exception {
+        Job job = getJob().withMetrics(Metrics.empty());
+        assertThat(job.status(), is(JobStatus.COMPLETE));
+    }
+
+    @Test
+    public void testWithBackoff() throws Exception {
+        Job job =  Job.backoff();
+        assertThat(job.computation().command(), containsString("sleep 10"));
+    }
+
+    @Test
+    public void testWithWaitForever() throws Exception {
+        Job job =  Job.waitForever();
+        assertThat(job.computation().command(), containsString("sleep 666"));
+    }
+
+
 }
