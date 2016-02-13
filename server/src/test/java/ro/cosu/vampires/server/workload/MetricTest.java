e54f2a07e9e55bf7f445f4777ae608323a119f89
Cosmin Dumitru <cosu@cosu.ro>
2016-02-14 00:07:53 +0100

    add metrictest

---
+++ server/src/test/java/ro/cosu/vampires/server/workload/MetricTest.java
@@ -0,0 +1,16 @@
+package ro.cosu.vampires.server.workload;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class MetricTest {
+
+    @Test
+    public void testBuilder() throws Exception {
+        Metric empty = Metric.empty();
+        assertThat(empty.values().size()  ,is(0));
+    }
+}
