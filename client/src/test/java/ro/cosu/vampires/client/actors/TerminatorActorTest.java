9ca4e6f468b5e5a322b898300cd03e9c40e3cf82
Cosmin Dumitru <cosu@cosu.ro>
2016-02-14 01:01:24 +0100

    add terminator test

---
+++ client/src/test/java/ro/cosu/vampires/client/actors/TerminatorActorTest.java
@@ -0,0 +1,45 @@
+package ro.cosu.vampires.client.actors;
+
+import akka.actor.ActorSystem;
+import akka.actor.PoisonPill;
+import akka.testkit.JavaTestKit;
+import akka.testkit.TestActorRef;
+import akka.testkit.TestProbe;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import scala.concurrent.duration.Duration;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created on 14-2-16.
+ */
+public class TerminatorActorTest {
+    private static ActorSystem system;
+
+    @BeforeClass
+    public static void setUp() {
+        system = ActorSystem.create();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        JavaTestKit.shutdownActorSystem(system);
+        system = null;
+    }
+
+    @Test
+    public void testTerminatorActor() throws Exception {
+        TestProbe testProbe = new TestProbe(system);
+
+        TestActorRef<TerminatorActor> terminator = TestActorRef.create(system, TerminatorActor.props(testProbe.ref()));
+        testProbe.ref().tell(PoisonPill.getInstance(), TestActorRef.noSender());
+
+        system.awaitTermination(Duration.create("1 second"));
+        assertThat(system.isTerminated(), is(true));
+
+    }
+
+}
