3769711379015cc758808ad41cf8791f2e05e520
Cosmin Dumitru <cosu@cosu.ro>
2016-02-14 22:22:10 +0100

    ignore empty jobs

--- server/src/main/java/ro/cosu/vampires/server/actors/WorkActor.java
+++ server/src/main/java/ro/cosu/vampires/server/actors/WorkActor.java
@@ -72,10 +72,9 @@ public class WorkActor extends UntypedActor {
     }
 
     private void receiveJob(Job job) {
-        if (!Computation.backoff().equals(job.computation())) {
+        if (!Computation.backoff().equals(job.computation()) ||
+                !Computation.empty().equals(job.computation())) {
             log.info("Work result from {}. pending {} ", job.metrics().metadata().get("host-hostname"), pendingJobs.size());
-            log.info("pending {}", pendingJobs.asMap());
-            log.info("invalidating {}", job.id());
             pendingJobs.invalidate(job.id());
             resultActor.forward(job, getContext());
 
