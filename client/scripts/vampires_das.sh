897a1f37829873aa857e9216f04151ffcf469422
Cosmin Dumitru <cosu@cosu.ro>
2016-02-14 19:57:14 +0100

    set sigar location

--- client/scripts/vampires_das.sh
+++ client/scripts/vampires_das.sh
@@ -5,6 +5,7 @@
 . /etc/bashrc
 . /etc/profile.d/modules.sh
 module load java/jdk-1.8.0
+export KAMON_SIGAR_FOLDER=/tmp
 
 #bind on infiniband
 export HOST=$(ip -f inet addr show ib0 |grep inet |awk '{print $2}'|cut -f1 -d/)
