ba182b030880d9ee133598a23d22b1081447406e
Cosmin Dumitru <cosu@cosu.ro>
2016-02-14 13:25:19 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+PRG="$0"
+# Need this for relative symlinks.
+while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`"/$link"
+    fi
+done
+SAVED="`pwd`"
+cd "`dirname \"$PRG\"`" >/dev/null
+
+git fetch --all
+git reset --hard origin/master
+./gradlew clean check installLocal
+
+cd "$SAVED" >/dev/null
+
+
