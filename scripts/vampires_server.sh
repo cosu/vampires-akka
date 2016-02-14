44aad8df85cafcd4ffc667dbeef34425c37ed45a
Cosmin Dumitru <cosu@cosu.ro>
2016-02-14 01:16:11 +0100

    silence curl

--- scripts/vampires_server.sh
+++ scripts/vampires_server.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-IP=`curl http://ip.cosu.ro/?ip |  tr -d '[[:space:]]'`
+IP=`curl -s http://ip.cosu.ro/?ip |  tr -d '[[:space:]]'`
 export BIND_HOST="0.0.0.0"
 export HOST=${IP}
 export SERVER_OPTS="-Dconfig.file=$1"
 shift
-echo "Binding to ${BIND_HOST} ${HOST}"
+echo "Vampires-Akka server: Binding to IP:${BIND_HOST} . Public host: ${HOST}"
 
 VAMPIRES_SERVER_HOME="${HOME}/vampires-akka/server/"
 
