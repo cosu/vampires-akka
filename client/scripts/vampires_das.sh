79cc51f1cfad39a82afe94a09e7e17cdad79a454
Cosmin Dumitru <cosu@cosu.ro>
2016-02-14 13:04:06 +0100

    debug das5 command

--- client/scripts/vampires_das.sh
+++ client/scripts/vampires_das.sh
@@ -13,24 +13,5 @@ export HOST=$(ip -f inet addr show ib0 |grep inet |awk '{print $2}'|cut -f1 -d/)
 export SERVER_IP=$1
 shift
 
-
-# Attempt to set APP_HOME
-# Resolve links: $0 may be a link
-PRG="$0"
-# Need this for relative symlinks.
-while [ -h "$PRG" ] ; do
-    ls=`ls -ld "$PRG"`
-    link=`expr "$ls" : '.*-> \(.*\)$'`
-    if expr "$link" : '/.*' > /dev/null; then
-        PRG="$link"
-    else
-        PRG=`dirname "$PRG"`"/$link"
-    fi
-done
-SAVED="`pwd`"
-cd "`dirname \"$PRG\"`/.." >/dev/null
-APP_HOME="`pwd -P`"
-cd "$SAVED" >/dev/null
-
-
-srun ${APP_HOME}/bin/client "$@"
+# this is hardcoded for now I have no clue for now how to get the app path. The gradle way  interferes with srun
+srun ${HOME}/vampires-akka-dist/client-1.0-SNAPSHOT/bin/client "$@"
