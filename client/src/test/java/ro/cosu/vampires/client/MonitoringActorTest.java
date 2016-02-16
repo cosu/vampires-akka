106ee612306b483a3e93b6ec25872329bdd21f74
Cosmin Dumitru <cosu@cosu.ro>
2016-02-16 23:55:46 +0100

    fix broken monitoring test

--- client/src/test/java/ro/cosu/vampires/client/MonitoringActorTest.java
+++ client/src/test/java/ro/cosu/vampires/client/MonitoringActorTest.java
@@ -4,6 +4,7 @@ import akka.actor.ActorSystem;
 import akka.testkit.JavaTestKit;
 import akka.testkit.TestActorRef;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,6 +15,7 @@ import scala.concurrent.Await;
 import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
 
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 
 import static org.hamcrest.core.IsNot.not;
@@ -43,12 +45,18 @@ public class MonitoringActorTest {
         TestActorRef<MonitoringActor> ref = TestActorRef.create(system, MonitoringActor
                 .props(MonitoringManager.getMetricRegistry()));
 
+        LocalDateTime start = LocalDateTime.now();
         int seconds = 2;
         Thread.sleep((seconds + 1) * 1000);
+        LocalDateTime stop = LocalDateTime.now();
 
         Computation computation = Computation.builder().command("test").id("test").build();
         Result result = Result.builder().duration(10).exitCode(0).output(new LinkedList<>())
-                .trace(Trace.empty())
+                .trace(Trace.builder().start(start).stop(stop)
+                        .cpuSet(Sets.newHashSet(1)).executor("foo").totalCpuCount(1)
+                        .executorMetrics(Metrics.empty())
+                    .build()
+                )
                 .build();
 
         Job jobWithoutMetrics = Job.builder().computation(computation).result(result)
@@ -58,6 +66,9 @@ public class MonitoringActorTest {
 
 
         final Future<Object> future = akka.pattern.Patterns.ask(ref, jobWithoutMetrics, 3000);
+
+
+
 
         Job job = (Job) Await.result(future, Duration.create("2 seconds"));
 
