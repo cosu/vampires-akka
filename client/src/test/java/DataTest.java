1ee8d118bbb9ffe9ab1c7b72ad86336dbebaf166
Cosmin Dumitru <cdumitru@sdl.com>
2016-02-18 17:22:59 +0100

    move data mangling to to test  package for now silence logger initialization

---
+++ client/src/test/java/DataTest.java
@@ -0,0 +1,72 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import ro.cosu.vampires.server.util.gson.AutoValueAdapterFactory;
+import ro.cosu.vampires.server.util.gson.ImmutableListTypeAdapterFactory;
+import ro.cosu.vampires.server.util.gson.ImmutableMapTypeAdapterFactory;
+import ro.cosu.vampires.server.writers.json.AllResults;
+import ro.cosu.vampires.server.writers.json.LocalDateTimeDeserializer;
+import ro.cosu.vampires.server.writers.json.LocalDateTimeSerializer;
+
+
+public class DataTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DataTest.class);
+
+    @Test
+    public void testName() throws Exception {
+
+        String homeDir = System.getProperty("user.home");
+        File file1 = Files.walk(Paths.get(homeDir), 1)
+                          .filter(Files::isRegularFile)
+                          .map(Path::toFile)
+                          .filter(file -> file.getName().endsWith(".json")).findFirst().get();
+
+        LOG.info("file {}", file1);
+        loadFromJson(file1);
+
+
+    }
+
+
+    private static AllResults loadFromJson(File  jsonFile) throws FileNotFoundException {
+        Gson gson = new GsonBuilder().setPrettyPrinting()
+                                     .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer())
+                                     .registerTypeAdapter(LocalDateTime.class ,new LocalDateTimeDeserializer())
+                                     .registerTypeAdapterFactory(new AutoValueAdapterFactory())
+                                     .registerTypeAdapterFactory(new ImmutableMapTypeAdapterFactory())
+                                     .registerTypeAdapter(ImmutableMap.class, ImmutableMapTypeAdapterFactory.newCreator())
+                                     .registerTypeAdapterFactory(new ImmutableListTypeAdapterFactory())
+                                     .create();
+
+        AllResults response = gson.fromJson(new FileReader(jsonFile), AllResults.class);
+
+        response.results().stream().forEach(job -> {
+
+//            System.out.println(job.hostMetrics());
+            job.hostMetrics().metrics().stream().forEach(metric -> {
+                LOG.info("time: {}", metric.time());
+                LOG.info("values: {}", metric.values());
+
+            });
+
+        });
+
+        return response;
+
+    }
+
+}
--- data/src/main/java/ro/cosu/vampires/data/Data.java
+++ data/src/main/java/ro/cosu/vampires/data/Data.java
@@ -1,28 +1,13 @@
 package ro.cosu.vampires.data;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import java.io.IOException;
+import java.util.logging.LogManager;
+
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.SQLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
-import ro.cosu.vampires.server.util.gson.AutoValueAdapterFactory;
-import ro.cosu.vampires.server.util.gson.ImmutableListTypeAdapterFactory;
-import ro.cosu.vampires.server.util.gson.ImmutableMapTypeAdapterFactory;
-import ro.cosu.vampires.server.writers.json.AllResults;
-import ro.cosu.vampires.server.writers.json.LocalDateTimeDeserializer;
-import ro.cosu.vampires.server.writers.json.LocalDateTimeSerializer;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.logging.LogManager;
 
 public class Data {
 
@@ -32,63 +17,36 @@ public class Data {
         SLF4JBridgeHandler.install();
     }
 
-    private static String jsonFile = "/home/cdumitru/results.json";
-    private static String jsonFile1 = "/home/cdumitru/results-all-2016-02-16T21:52:07.349.json";
-
-    private static AllResults loadFromJson() throws FileNotFoundException {
-        Gson gson = new GsonBuilder().setPrettyPrinting()
-                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer())
-                .registerTypeAdapter(LocalDateTime.class ,new LocalDateTimeDeserializer())
-                .registerTypeAdapterFactory(new AutoValueAdapterFactory())
-                .registerTypeAdapterFactory(new ImmutableMapTypeAdapterFactory())
-                .registerTypeAdapter(ImmutableMap.class, ImmutableMapTypeAdapterFactory.newCreator())
-                .registerTypeAdapterFactory(new ImmutableListTypeAdapterFactory())
-                .create();
-
-        AllResults response = gson.fromJson(new FileReader(jsonFile1), AllResults.class);
-
-        response.results().stream().forEach(job -> {
-            job.hostMetrics().metrics().stream().forEach(metric -> {
-                LOG.info("time: {}", metric.time());
-                LOG.info("values: {}", metric.values());
-
-            });
-
-        });
-
-        return response;
-
-    }
 
 
     private static final Logger LOG = LoggerFactory.getLogger(Data.class);
 
     public static void main(String[] args) throws IOException {
 
-        loadFromJson();
+
         SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> logData = sc.textFile(jsonFile).cache();
-
-
-        long numAs = logData.filter(s -> s.contains("a")).count();
-
-        long numBs = logData.filter(s -> s.contains("b")).count();
-
-        LOG.info("Lines with a: " + numAs + ", lines with b: " + numBs);
-
-
-        SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
-        DataFrame df = sqlContext.read().json(jsonFile);
-
-        AllResults allResults = loadFromJson();
-//        sqlContext.createDataFrame()
-        df.show();
-
-        df.printSchema();
-
-        df.filter("result.duration < 4517").show();
+//        JavaRDD<String> logData = sc.textFile(jsonFile).cache();
 //
+//
+//        long numAs = logData.filter(s -> s.contains("a")).count();
+//
+//        long numBs = logData.filter(s -> s.contains("b")).count();
+//
+//        LOG.info("Lines with a: " + numAs + ", lines with b: " + numBs);
+//
+//
+//        SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
+//        DataFrame df = sqlContext.read().json(jsonFile);
+//
+//        AllResults allResults = loadFromJson();
+////        sqlContext.createDataFrame()
+//        df.show();
+//
+//        df.printSchema();
+//
+//        df.filter("result.duration < 4517").show();
+////
 
 
     }
