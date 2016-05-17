ec407af7e4d60273c60670b238b26ed23326ec0f
Cosmin Dumitru <cosu@cosu.ro>
2016-05-17 22:21:21 +0200

    adjust terminator test

---
+++ server/scripts/listProviders.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+set -o nounset
+
+api_server="http://localhost:4567"
+
+
+curl -s ${api_server}/providers
--- server/src/test/java/ro/cosu/vampires/server/actors/TerminatorTest.java
+++ server/src/test/java/ro/cosu/vampires/server/actors/TerminatorTest.java
@@ -37,21 +37,6 @@ import scala.concurrent.duration.Duration;
 
 public class TerminatorTest extends AbstractActorTest {
     @Test
-    public void testStart() throws Exception {
-        final TestProbe testProbe = new TestProbe(system);
-
-        TestActorRef<Terminator> testActorRef = TestActorRef.create(system, Terminator
-                .props(), "terminator");
-
-        testActorRef.tell(new ResourceControl.Up(), testProbe.ref());
-
-        system.stop(testProbe.ref());
-
-        Await.result(system.whenTerminated(), Duration.create("1 second"));
-
-    }
-
-    @Test
     public void testShutdown() throws Exception {
         final TestProbe testProbe = new TestProbe(system);
 
